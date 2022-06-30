903b53b577c765243cfe81b3ec4437dca383272d
AJlekcaHDpRadmir <88597582+AJlekcaHDpRadmir@users.noreply.github.com>
2022-07-01 02:21:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var car = new Array(500); // create an empty array with length 45
+for(var i = 400; i < car.length; i++){
+  document.write('<div class="single-goods">' +
+    '<img src="https://rdmr.ru/img/vehicle_images/'+(i + 1)+'_1.png">'
+    + '<p>id: '+(i + 1)+'</p>'+'</div>'); 
+}
