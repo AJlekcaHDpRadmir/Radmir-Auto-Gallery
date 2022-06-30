d7816e9a3f7bf3ec0be96a76372889b73bbfe720
AJlekcaHDpRadmir <88597582+AJlekcaHDpRadmir@users.noreply.github.com>
2022-07-01 02:23:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var car = new Array(500); // create an empty array with length 45
+var car = new Array(603); // create an empty array with length 45
 for(var i = 400; i < car.length; i++){
   document.write('<div class="single-goods">' +
     '<img src="https://rdmr.ru/img/vehicle_images/'+(i + 1)+'_1.png">'
