daeac8be1ac112b95b4d0cf5a8e1916f379f92d0
AJlekcaHDpRadmir <88597582+AJlekcaHDpRadmir@users.noreply.github.com>
2022-07-01 13:11:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-var car = new Array(603); // create an empty array with length 45
-for(var i = 400; i < car.length; i++){
-  document.write('<div class="single-goods">' +
-    '<img src="https://rdmr.ru/img/vehicle_images/'+(i + 1)+'_1.png">'
-    + '<p>id: '+(i + 1)+'</p>'+'</div>'); 
-}
+/*var k = j = 400;
+while ( j <= 500 ){
+    /*var t = new Image();  
+    
+    t.onload = function () {
+      console.log('картинка существует', j)
+    };
+    t.onerror = function(){
+      console.log('картинка не существует')
+    };
+    t.src = 'https://rdmr.ru/img/vehicle_images/' + j + '_1.png';
+} */
+
+var i = 400;
+var n = 500;
+var k = i;
+var t = new Image();  
+setInterval(function() {
+  k = i;
+  t.src = 'https://rdmr.ru/img/vehicle_images/' + k + '_1.png';  
+  if (i < n) {
+    t.onload = function () {
+      document.write('<div class="single-goods">' +
+      '<img src="https://rdmr.ru/img/vehicle_images/'+(k)+'_1.png">'
+      + '<p>id: '+(k)+'</p>'+'</div>')
+    };
+    t.onerror = function(){
+      console.log('картинка не существует', k)
+    };
+  }
+  i++;
+}, 1000);
