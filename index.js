bef6cc970c9b6b807a9fe451e9a059af50e21453
AJlekcaHDpRadmir <88597582+AJlekcaHDpRadmir@users.noreply.github.com>
2022-07-01 13:35:51 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,22 +11,20 @@ while ( j <= 500 ){
     t.src = 'https://rdmr.ru/img/vehicle_images/' + j + '_1.png';
 } */
 
-var i = 400;
-var n = 500;
-var k = i;
+var i = 399;
+var n = 20000;
+var k = 0;
 var t = new Image();  
 setInterval(function() {
   k = i;
   t.src = 'https://rdmr.ru/img/vehicle_images/' + k + '_1.png';  
   if (i < n) {
     t.onload = function () {
-      document.write('<div class="single-goods">' +
-      '<img src="https://rdmr.ru/img/vehicle_images/'+(k)+'_1.png">'
-      + '<p>id: '+(k)+'</p>'+'</div>')
+      document.write('<div class="single-goods" style="border: 1px solid black; margin: 10px; float: left; text-align: center;">' +
+      '<img style="width: 400px;" src="https://rdmr.ru/img/vehicle_images/'+(k)+'_1.png">'
+      + '<p>id: '+(k)+ '</p>'+'</div>')
     };
-    t.onerror = function(){
-      console.log('картинка не существует', k)
-    };
+    t.onerror = function(){};
   }
   i++;
-}, 100);
+}, 200);
